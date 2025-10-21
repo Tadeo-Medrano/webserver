d48f24135183c99a243861b84e09b9e33959a076
Tadeo-Medrano <tadeo.ingsist@gmail.com>
2025-10-21 13:39:31 -0600

    ¨Nuevo¨

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 import express from "express";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
+const port = 8080;
 
-app.get("/", (req, res) => {
-  res.send("Silksong para GOTY");
+// Middleware: servir contenido estático
+app.use(express.static("public"));
+
+app.get("/generic", (req, res) => {
+  res.sendFile(__dirname + "/public/generic.html");
 });
 
-app.listen(8080);
+app.get("/elements", (req, res) => {
+  res.sendFile(__dirname + "/public/elements.html");
+});
+
+app.get("/index", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+// Ruta comodín (404)
+app.get(/.*/, (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
